703eb6601d980cd8def98d294fe7e7bc435489aa
Daniel Hood <dhood@phoslap.blizzardtracks.com>
2013-11-29 16:16:56 -0500

    Added v2.0 of the midi2cv controller program, however incomplete. The PIC18F2431 cannot have more than 3 independent PWM outs, preventing us from implementing the 2nd PITCH/VEL/MOD chanel.

---
+++ v1.0/pic/PIC18F2431/asm/midi.cv.v2.X/build/default/production/src/midi.o.d
@@ -0,0 +1,5 @@
+build/default/production/src/midi.o : \
+	C:/Users/dhood/MPLABXProjects/PIC18F2431/asm/midi.cv.X/src/midi.asm \
+	C:/Program\ Files\ (x86)/Microchip/MPLABX/mpasmx/P18F2431.INC \
+	C:/Users/dhood/MPLABXProjects/PIC18F2431/asm/midi.cv.X/inc/vars.inc \
+
