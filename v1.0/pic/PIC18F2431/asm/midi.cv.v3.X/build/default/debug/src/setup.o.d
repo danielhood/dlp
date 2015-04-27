1ee59b8988492efb09b3dacf82bf98c4b4449e31
Daniel Hood <danielhood@hotmail.com>
2015-04-27 09:42:52 -0400

    A bunch of old change from midi.cv 3.0

--- v1.0/pic/PIC18F2431/asm/midi.cv.v3.X/build/default/debug/src/setup.o.d
+++
@@ -1,5 +0,0 @@
-build/default/debug/src/setup.o : \
-	C:/Users/dhood/git/danielhood/dlp/v1.0/pic/PIC18F2431/asm/midi.cv.v3.X/src/setup.asm \
-	C:/Program\ Files\ (x86)/Microchip/MPLABX/mpasmx/P18F2431.INC \
-	C:/Users/dhood/git/danielhood/dlp/v1.0/pic/PIC18F2431/asm/midi.cv.v3.X/inc/vars.inc \
-
