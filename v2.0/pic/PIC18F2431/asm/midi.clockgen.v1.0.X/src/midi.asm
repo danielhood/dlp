6939da222f5b510ca518e9234498e1e2b15f8faa
Daniel Hood <danielhood@hotmail.com>
2019-03-23 21:28:00 -0400

    Created midi.clockgen from copy of midi.clock

---
+++ v2.0/pic/PIC18F2431/asm/midi.clockgen.v1.0.X/src/midi.asm
@@ -0,0 +1,252 @@
+; MIDI Processing Routines (Main code loop)
+
+#include p18f2431.inc
+#include inc/vars.inc
+
+; ------------------------------------------------------------------------------
+; Export global code refs
+;
+	global	_MAIN
+
+	extern LFO_SINE_DATA_24
+	extern LFO_SINE_DATA_32
+	extern LFO_SINE_DATA_96
+
+
+
+; ------------------------------------------------------------------------------
+; Converts a 7 bit value in W and scales the result returining it in W
+; Register W is expected to contain the base address of the CV value lookup table.
+;	Use program memory lookup.
+;	EEPROM lookups seemed to only return 00's.
+;
+;_SCALE_CV_NOTE	macro
+;	addlw	low CV_PITCH_DATA
+;	movwf	TBLPTRL		; Address to read
+;	tblrd	*
+;	movf	TABLAT,W	; W = table data
+;	endm
+
+; -----------------------------------------------------------------------------
+; Lookup value in SINE_24_DATA table
+; W must contain a value between 0 and 23
+;
+_LOOKUP_LFO_SINE_24 macro
+	;movlw	high LFO_SINE_DATA_24
+	;movwf	TBLPTRH		; Select the program block with data (0x3000)
+	addlw	low LFO_SINE_DATA_24	; Add offset to table base
+	movwf	TBLPTRL
+	tblrd	*
+	movf	TABLAT,W			; W = table data
+	endm
+
+; -----------------------------------------------------------------------------
+; Lookup value in SINE_32_DATA table
+; W must contain a value between 0 and 31
+;
+_LOOKUP_LFO_SINE_32 macro
+	;movlw	high LFO_SINE_DATA_32
+	;movwf	TBLPTRH		; Select the program block with data (0x3000)
+	addlw	low LFO_SINE_DATA_32	; Add offset to table base
+	movwf	TBLPTRL
+	tblrd	*
+	movf	TABLAT,W			; W = table data
+	endm
+
+; -----------------------------------------------------------------------------
+; Lookup value in SINE_96_DATA table
+; W must contain a value between 0 and 95
+;
+_LOOKUP_LFO_SINE_96 macro
+	;movlw	high LFO_SINE_DATA_96
+	;movwf	TBLPTRH		; Select the program block with data (0x3000)
+	addlw	low LFO_SINE_DATA_96	; Add offset to table base
+	movwf	TBLPTRL
+	tblrd	*
+	movf	TABLAT,W			; W = table data
+	endm
+
+; ------------------------------------------------------------------------------
+; Macro to split the 7 bits across the high and low PWM output registers
+; by putting them into the TMP_CV_BYTE_L and TMP_CV_BYTE_H
+;	The 7th bit should end up in the TMP_CV_BYTE_H
+;	The remaining 6 bits should be in the upper 6 bits of TMP_CV_BYTE_L
+;	Upon return:
+;		TMP_CV_BYTE_H should be placed into PDCxH: '--00000d'
+;		TMP_CV_BYTE_L should be placed into PDCxL: 'dddddd00'
+;
+_SPLIT_CV_VALUE	macro
+	movwf	TMP_CV_BYTE_L	; W should contain the 7bit CV value (0..127)
+	bcf	STATUS,C	; Ensure C = 0
+	rlncf	TMP_CV_BYTE_L,F
+	rlcf	TMP_CV_BYTE_L,F	; pushes the msbit of 7bit value into C
+	clrf	TMP_CV_BYTE_H
+	rlcf	TMP_CV_BYTE_H	; rolls on the C bit 
+	endm
+
+_SPLIT_CV_VALUE2	macro
+	;rlncf	WREG		; Double the value since we're using a period of 256 instead of 128
+	movwf	TMP_CV_BYTE_L	; W should contain the 8bit CV value
+	andlw	0xC0
+	movwf	TMP_CV_BYTE_H	;
+	rlncf	TMP_CV_BYTE_L,F
+	rlncf	TMP_CV_BYTE_L,F	; pushes the msbit of 7bit value into C
+	swapf	TMP_CV_BYTE_H,F
+	rrncf	TMP_CV_BYTE_H,F
+	rrncf	TMP_CV_BYTE_H,F
+	endm
+
+; ------------------------------------------------------------------------------
+; Main code
+;
+.MAIN	code			; Relocatable code for main application
+_MAIN:
+	clrf	PORTA		; Clear PORTA
+	clrf	PORTB		; Clear PORTB
+	clrf	PORTC		; Clear PORTC
+
+	movlw	b'00000000'	; Set the high byte to 0
+	movwf	PDC0H
+	movwf	PDC1H
+	movwf	PDC2H
+
+	; Make sure initial output state starts at 0 otherwise song start may not fire properly
+	setf	CV_GATE				; Force all clocks outs to 0 (note inversion)
+	setf	CLOCK_DIVIDER
+
+
+_LOOP:
+_UPDATE_CV1:
+	btfss	CV_FLAGS,CVF_PITCH
+	goto	_UPDATE_CV2
+	movf	CLOCK_COUNTER_96,W
+	_LOOKUP_LFO_SINE_96		; returning result in W
+	movwf	CV_PITCH_TARGET		; set new target
+	movlw	DELAY_96		; reset delay counter
+	movwf	CV_PITCH_DELAY
+	bcf	CV_FLAGS,CVF_PITCH
+
+_UPDATE_CV2:
+	btfss	CV_FLAGS,CVF_VELOCITY
+	goto	_UPDATE_CV3
+	movf	CLOCK_COUNTER_24,W
+	_LOOKUP_LFO_SINE_24
+	movwf	CV_VEL_TARGET		; set new target
+	movlw	DELAY_24		; reset delay counter
+	movwf	CV_VEL_DELAY
+	bcf	CV_FLAGS,CVF_VELOCITY
+
+_UPDATE_CV3:
+	btfss	CV_FLAGS,CVF_MOD
+	goto	_CHECK_PITCH
+	movf	CLOCK_COUNTER_32,W
+	_LOOKUP_LFO_SINE_32
+	movwf	CV_MOD_TARGET		; set new target
+	movlw	DELAY_32		; reset delay counter
+	movwf	CV_MOD_DELAY
+	bcf	CV_FLAGS,CVF_MOD
+
+	;movff	CV_GATE,PORTC
+	;goto	_LOOP
+
+	;movff	CLOCK_DIVIDER,PORTC
+	;goto	_LOOP
+
+
+_CHECK_PITCH:
+	movf	CV_PITCH_TARGET,W	; Check to see if target matches curvalue
+	cpfseq	CV_PITCH
+	goto	_UPDATE_PITCH_CHECK
+	goto	_CHECK_VEL
+_UPDATE_PITCH_CHECK:
+	decfsz	CV_PITCH_DELAY,F	; Check counter
+	goto	_CHECK_VEL
+	movf	CV_PITCH,W
+	cpfsgt	CV_PITCH_TARGET		; increment or decrement?
+	goto	_DEC_PITCH
+	incf	CV_PITCH,F
+	goto	_UPDATE_PITCH
+_DEC_PITCH:
+	decf	CV_PITCH,F
+_UPDATE_PITCH:
+	movf	CV_PITCH,W
+	_SPLIT_CV_VALUE2
+	movff	TMP_CV_BYTE_H,PDC0H
+	movff	TMP_CV_BYTE_L,PDC0L
+	movlw	DELAY_96		; reset delay counter
+	movwf	CV_PITCH_DELAY
+
+_CHECK_VEL:
+	movf	CV_VEL_TARGET,W	; Check to see if target matches curvalue
+	cpfseq	CV_VELOCITY
+	goto	_UPDATE_VEL_CHECK
+	goto	_CHECK_MOD
+_UPDATE_VEL_CHECK:
+	decfsz	CV_VEL_DELAY,F	; Check counter
+	goto	_CHECK_MOD
+	movf	CV_VELOCITY,W
+	cpfsgt	CV_VEL_TARGET		; increment or decrement?
+	goto	_DEC_VEL
+	incf	CV_VELOCITY,F
+	goto	_UPDATE_VEL
+_DEC_VEL:
+	decf	CV_VELOCITY,F
+_UPDATE_VEL:
+	movf	CV_VELOCITY,W
+	_SPLIT_CV_VALUE2
+	movff	TMP_CV_BYTE_H,PDC1H
+	movff	TMP_CV_BYTE_L,PDC1L
+	movlw	DELAY_24		; reset delay counter
+	movwf	CV_VEL_DELAY
+
+_CHECK_MOD:
+	movf	CV_MOD_TARGET,W	; Check to see if target matches curvalue
+	cpfseq	CV_MOD
+	goto	_UPDATE_MOD_CHECK
+	goto	_UPDATE_GATES
+_UPDATE_MOD_CHECK:
+	decfsz	CV_MOD_DELAY,F	; Check counter
+	goto	_UPDATE_GATES
+	movf	CV_MOD,W
+	cpfsgt	CV_MOD_TARGET		; increment or decrement?
+	goto	_DEC_MOD
+	incf	CV_MOD,F
+	goto	_UPDATE_MOD
+_DEC_MOD:
+	decf	CV_MOD,F
+_UPDATE_MOD:
+	movf	CV_MOD,W
+	_SPLIT_CV_VALUE2
+	movff	TMP_CV_BYTE_H,PDC2H
+	movff	TMP_CV_BYTE_L,PDC2L
+	movlw	DELAY_32		; reset delay counter
+	movwf	CV_MOD_DELAY
+
+_UPDATE_GATES:
+	movf	CV_GATE,W
+	rlncf	WREG		; Shift 16ths to open fist bit to store base clock
+	andlw	0xFE		; bit could be dirty
+	movwf	CV_GATE_TMP
+	movf	CLOCK_DIVIDER,W
+	rrncf	WREG		; Divide base clock (tripples) by 4
+	rrncf	WREG
+	andlw	0x01
+	addwf	CV_GATE_TMP,W	; Inject base clock into port set
+	andlw	0x0F		; Clear out top bit for Song Start
+	movwf	CV_GATE_TMP	; store back to tmp
+	movf	SONG_START_INV,W ; load song start flag (will be 0xFF or 0x00)
+	andlw	0x10		; Mask out bit
+	addwf	CV_GATE_TMP,W	; Inject song start
+	;addlw	0x00	; test
+	comf	WREG,W	; Invert the bits so ports start at 1
+	movwf	PORTC		; Apply gate settings directly to PORTC
+
+	; TODO: cycle through a sin or triangle way to generate clock sync'd LFO's on CV outs
+	; TODO: revisit MIDI state tracking to see if we can do it efficiently
+	; TODO: test main MIDI2CV program to see if it handles clock sync
+
+	goto	_LOOP
+
+	end
+
+
---
+++ v2.0/pic/PIC18F2431/asm/midi.clockgen.v1.0.X/src/setup.asm
@@ -0,0 +1,111 @@
+#include p18f2431.inc
+#include inc/vars.inc
+
+; ------------------------------------------------------------------------------
+; Export global code refs
+;
+	global	_SETUP
+
+	extern LFO_SINE_DATA_24
+	extern LFO_SINE_DATA_32
+	extern LFO_SINE_DATA_96
+
+; ------------------------------------------------------------------------------
+; Setup routine
+;
+.SETUP	code			; Relocatable code for setup
+_SETUP:
+
+; Init I/O ports
+	movlw	0x00		; All Digital I/O on PORTA
+	movwf	ANSEL0
+
+	movlw	b'00000001'	; PORTA:	RA0 - Switch Input for MIDI Mode (Pitch/Mod vs Drum Trigger)
+	movwf	TRISA		; 		RA1:4 - Output (Unused)
+				;		RA5 - Doesn't exist on this chip
+				;		RA6:7 - Crystal Osc
+
+	movlw	b'00000000'	; PORTB:	RB0,2,5 - Digital Out (Unused)
+	movwf	TRISB		;		RB1,3,4 - PWM Out 1,3,5 (CV)
+				;		RB6:7 - PGC/PGD
+
+	movlw	b'11000000'	; PORTC
+	movwf	TRISC		;		RC0:4 - Ditigal Out (Gates 1 - 5)
+				;		RC5   - Digital Out (Unused)
+				;		RC6:7 - Serial TX/RX (must both be set to 1)
+
+; PWM Config (for CV out)
+	movlw	b'00000010'	; No Pre-scale, No Post-scale, Continuous w/no interrupts
+	movwf	PTCON0		; Timer clock runs at FOSC/4 = 5MHz
+
+	movlw	b'10000000'	; Time base enabled, count up (ignored)
+	movwf	PTCON1
+
+	movlw	b'01110111'	; Enable only odd PWM pins (RB1,3,4/PWM1,3,5); all in independent mode
+	movwf	PWMCON0
+
+	movlw	b'00000000'	; SE trigger postscale; trigger on count up; Update Duty/Period regs; Async PWM out
+	movwf	PWMCON1
+
+	clrf	PTPERH		; Set period register to 128, the tightest period for the control we need
+	movlw	0xFF		; allows a 1 to 1 mapping for the CC registers (no need for EEPROM lookup)
+	movwf	PTPERL		; The only reason we'd change this is if we need more control for the CV tone
+				; With a timer clock at 5MHz, the base PWM freq is 5Mhz/128 = 39.0625KHz
+				; RC = 1/2xPixf =
+				;	R in K; C in uF; f in KHz
+				;		R = .390 K
+				;		f = 39.0625 KHz
+				;		C = 0.0104 uF (0x47+uF seemed to be requried)
+
+
+; Serial RX Config (for MIDI IN)
+	bcf	TXSTA,SYNC	; Asynchronous
+	bcf	TXSTA,BRGH	; Low spped BRG
+	bcf	BAUDCON,BRG16	; 8 bit BRG
+	bcf	BAUDCON,ABDEN	; Disable auto-baud detect
+	clrf	SPBRGH		; Clear BRG high bit
+	movlw	0x09		; Set BRG to 9 = 2MHz/31.25KHZ/64-1
+	movwf	SPBRG
+	bcf	RCSTA,RX9	; 8-bit data
+	bsf	RCSTA,CREN	; Enable RX
+
+	bsf	PIE1,RCIE	; Enable Serial RX Interrupt
+	bsf	IPR1,RCIP	; Set to High Priority
+
+	bsf	RCSTA,SPEN	; Enable Serial IO
+
+; Setup vars
+	clrf	CUR_BYTE
+	clrf	MSG_COUNT
+
+	clrf	CV_FLAGS
+	clrf	CV_PITCH
+	clrf	CV_VELOCITY
+	clrf	CV_MOD
+	clrf	CV_GATE
+
+	movlw	_MOFFSET_NONE	; Set inital midi state
+	movwf	MIDI_STATE
+	
+	movlw	0x01
+	movwf	MSG_CHAN	; Default to channel 1
+
+
+	movlw	0xF7		; Set SysEx data to deault state
+	movwf	SYS_EX_DATA
+
+; Setup Table memory pointers
+	movlw	upper LFO_SINE_DATA_24		; Will be the same for all tables, only need to set H and L bytes each lookup
+	movwf	TBLPTRU		; Select the program block with data (0x3000)
+	movlw	high LFO_SINE_DATA_24
+	movwf	TBLPTRH		; Select the program block with data (0x3000)
+
+; Enable Interrupts
+	bsf	RCON,IPEN	; Enable interrupt priorities
+	bcf	INTCON,GIEL	; Disable low-prority interrupts
+	bsf	INTCON,GIEH	; Enable high-priority interrupts
+
+	return
+
+	end
+
